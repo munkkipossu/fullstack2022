8938e4f957b28a42d2978fe88e89c88a4b93cc4e
Teemu <damunq@gmail.com>
2022-07-21 18:03:18 +0300

    modify lesson code

--- part1/lesson/src/App.js
+++ part1/lesson/src/App.js
@@ -1,27 +1,43 @@
-const Hello = (props) => {
-    return (
-          <div>
-            <p>Hello {props.name}</p>
-          </div>
-    )
-}
+import { useState } from 'react'
 
-const Footer = () => {
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div> 
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
     <div>
-      greeting app created by <a href="https://github.com/munkkipossu">mp</a>
+      button press history: {props.allClicks.join(' ')}
     </div>
   )
 }
 
+const Button = ({ handleClick, text }) => (  <button onClick={handleClick}>    {text}  </button>)
+
 const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Huti" />
-      <Hello name="Vinny" />
-      <Footer /> 
-    </>
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
   )
 }
+
 export default App
