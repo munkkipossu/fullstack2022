05792db74a225749fe0ee840b7b0efecb372d4c4
Teemu <damunq@gmail.com>
2022-07-21 20:02:49 +0300

    add solution to exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,15 +11,28 @@ const Datum = ({name, value}) => (
   <p>{name} {value}</p>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = () => good + neutral + bad
+  const average = () => (good - bad)/total()
+  const positive = () => good/total()*100
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <Datum name="good" value={good} />
+      <Datum name="neutral" value={neutral} />
+      <Datum name="bad" value={bad} />
+      <p>all {total()}</p>
+      <p>average {average()}</p>
+      <p>positive {positive()}%</p>
+    </div>
+  )
+}
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = () => good + neutral + bad
-  const average = () => (good - bad)/total()
-  const positive = () => good/total()*100
-  
   const increaseGood = () => { setGood(good + 1) } 
   const increaseNeutral = () => { setNeutral(neutral + 1) } 
   const increaseBad = () => { setBad(bad + 1) } 
@@ -30,13 +43,7 @@ const App = () => {
       <Button handleClick={increaseGood} text="good" />
       <Button handleClick={increaseNeutral} text="neutral" />
       <Button handleClick={increaseBad} text="bad" />
-      <h1>statistics</h1>
-      <Datum name="good" value={good} />
-      <Datum name="neutral" value={neutral} />
-      <Datum name="bad" value={bad} />
-      <p>all {total()}</p>
-      <p>average {average()}</p>
-      <p>positive {positive()}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
