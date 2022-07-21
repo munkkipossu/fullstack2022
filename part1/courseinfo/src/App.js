1fb7f949bad62b38e340f722bdeaa4b7d77459b4
Teemu <damunq@gmail.com>
2022-07-21 15:09:36 +0300

    fix formatting for exercise 1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-    <h1>{props.message}</h1>
+    <h1>{props.course}</h1>
   )
 
 }
@@ -9,13 +9,13 @@ const Content = (props) => {
   return (
     <div>
       <p>
-      {props.part1} {props.exercises1}
+        {props.part1} {props.exercises1}
       </p>
       <p>
-      {props.part2} {props.exercises2}
+        {props.part2} {props.exercises2}
       </p>
       <p>
-      {props.part3} {props.exercises3}
+        {props.part3} {props.exercises3}
       </p>
     </div>
   )
@@ -37,7 +37,7 @@ const App = () => {
 
   return (
     <div>
-      <Header message={course} />
+      <Header course={course} />
       <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
