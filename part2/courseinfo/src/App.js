6de68ccbf40a872c435bbbef6d0ffa78acf076d0
Teemu <damunq@gmail.com>
2022-07-22 14:24:21 +0300

    add soltuion for 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,24 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Total = ({ sum }) => <p><b>Number of exercises {sum}</b></p>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </div>
-
-const Course = ({course}) =>
- <>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={course.parts.reduce((s, part) =>  s + part.exercises, 0)} />
- </>
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
@@ -73,4 +53,5 @@ const App = () => {
     </div>
   )
 }
+
 export default App
