ff7ba428cbfe2cb24530558b92d9097a10355301
Teemu <damunq@gmail.com>
2022-10-21 21:04:34 +0300

    add solution to 4.10

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -19,6 +19,32 @@ test('api returns initial blogs', async () => {
   expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
+test('blog has id property', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
+test('adding a blog is saved', async () => {
+  const title = 'Po-ta-toes - smash \'em'
+  const newBlog = {
+    title: title,
+    author: 'SamWise',
+    url: 'www.bagend.me/recepies/potates/mashem',
+    likes: 3123213,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(response.body.map(blog => blog.title)).toContain(title)
+}, 100000)
+
 afterAll(() => {
   mongoose.connection.close()
 })
