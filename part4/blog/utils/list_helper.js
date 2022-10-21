a042ba53ac8e38793dba0415c53cf44bef59b2ff
Teemu <damunq@gmail.com>
2022-10-21 17:33:05 +0300

    add solution to 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,6 +6,22 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0)
+    return null
+  
+  favourites = [...blogs]
+  favourites.sort((x, y) => x.likes < y.likes ? 1 : -1)
+
+  return {
+    title: favourites[0].title,
+    author: favourites[0].author,
+    likes: favourites[0].likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy,
+  totalLikes,
+  favouriteBlog,
 }
