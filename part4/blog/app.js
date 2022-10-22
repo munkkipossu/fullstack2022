d3baf6a0b87cf780ffd4dc52b2c139d996ba7ff9
Teemu <damunq@gmail.com>
2022-10-22 11:42:48 +0300

    add solutio to 4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -6,6 +6,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const mongoose = require('mongoose')
 
@@ -16,6 +17,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 module.exports = app
